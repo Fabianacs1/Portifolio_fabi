8dd73c8cb8b6a718004a650a2704bde3d9bdd71a
Fabiana Carvalho <160019337+Fabianacs1@users.noreply.github.com>
2024-06-27 07:52:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
     const username = 'Fabianacs1'; 
-    const token = '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';
-
 
     const links = document.querySelectorAll('.nav__link');
 
@@ -70,41 +68,37 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     ];
 
-    fetch(`https://api.github.com/users/${username}/repos`, {
-        headers: {
-            Authorization: `token ${token}`
-        }
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error(`Erro ao buscar repositórios: ${response.status} - ${response.statusText}`);
-        }
-        return response.json();
-    })
-    .then(repos => {
-        const reposList = document.getElementById('github-repos-list');
+    fetch(`https://api.github.com/users/${username}/repos`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Erro ao buscar repositórios: ${response.status} - ${response.statusText}`);
+            }
+            return response.json();
+        })
+        .then(repos => {
+            const reposList = document.getElementById('github-repos-list');
 
-        projects.forEach(project => {
-            const projectItem = document.createElement('div');
-            projectItem.className = 'project pure-u-1 pure-u-md-1-2';
-            projectItem.innerHTML = `
-                <h3 class="project__title">${project.name}</h3>
-                <p class="project__description">${project.description}</p>
-                <a href="${project.url}" target="_blank" class="project__link button">Ver Projeto</a>
-            `;
-            reposList.appendChild(projectItem);
-        });
+            projects.forEach(project => {
+                const projectItem = document.createElement('div');
+                projectItem.className = 'project pure-u-1 pure-u-md-1-2';
+                projectItem.innerHTML = `
+                    <h3 class="project__title">${project.name}</h3>
+                    <p class="project__description">${project.description}</p>
+                    <a href="${project.url}" target="_blank" class="project__link button">Ver Projeto</a>
+                `;
+                reposList.appendChild(projectItem);
+            });
 
-        repos.forEach(repo => {
-            const repoItem = document.createElement('div');
-            repoItem.className = 'project pure-u-1 pure-u-md-1-2';
-            repoItem.innerHTML = `
-                <h3 class="project__title">${repo.name}</h3>
-                <p class="project__description">${repo.description || 'Sem descrição'}</p>
-                <a href="${repo.html_url}" target="_blank" class="project__link button">Ver Projeto</a>
-            `;
-            reposList.appendChild(repoItem);
-        });
-    })
-    .catch(error => console.error('Erro ao buscar repositórios do GitHub:', error));
+            repos.forEach(repo => {
+                const repoItem = document.createElement('div');
+                repoItem.className = 'project pure-u-1 pure-u-md-1-2';
+                repoItem.innerHTML = `
+                    <h3 class="project__title">${repo.name}</h3>
+                    <p class="project__description">${repo.description || 'Sem descrição'}</p>
+                    <a href="${repo.html_url}" target="_blank" class="project__link button">Ver Projeto</a>
+                `;
+                reposList.appendChild(repoItem);
+            });
+        })
+        .catch(error => console.error('Erro ao buscar repositórios do GitHub:', error));
 });
