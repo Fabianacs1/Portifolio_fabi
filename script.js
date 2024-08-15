02f0d227fbb7bf698c32aefd391fc3a8de878bae
Fabiana Carvalho <160019337+Fabianacs1@users.noreply.github.com>
2024-08-15 15:33:45 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const username = 'Fabianacs1'; 
+    const username = 'Fabianacs1';
     const token = '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';
 
+    fetch(`https://api.github.com/users/${username}/repos`, {
+        headers: {
+            Authorization: `token ${token}`
+        }
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`Erro ao buscar repositórios: ${response.status} - ${response.statusText}`);
+        }
+        return response.json();
+    })
+    .then(repos => {
+        const reposList = document.getElementById('github-repos-list');
 
-    const links = document.querySelectorAll('.nav__link');
-
-    links.forEach(link => {
-        link.addEventListener('click', function(event) {
-            event.preventDefault();
-            const targetId = this.getAttribute('href').substring(1);
-            const targetSection = document.getElementById(targetId);
-
-            if (targetSection) {
-                targetSection.scrollIntoView({ behavior: 'smooth' });
-            }
+        repos.forEach(repo => {
+            const repoItem = document.createElement('div');
+            repoItem.className = 'project pure-u-1 pure-u-md-1-2';
+            repoItem.innerHTML = `
+                <h3 class="project__title">${repo.name}</h3>
+                <p class="project__description">${repo.description || 'Sem descrição'}</p>
+                <a href="${repo.html_url}" target="_blank" class="project__link button">Ver Projeto</a>
+            `;
+            reposList.appendChild(repoItem);
         });
-    });
+    })
+    .catch(error => console.error('Erro ao buscar repositórios do GitHub:', error));
+});
 
     const sections = document.querySelectorAll('.section');
 
@@ -107,16 +120,16 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     })
     .catch(error => console.error('Erro ao buscar repositórios do GitHub:', error));
-});
+;
 document.addEventListener("DOMContentLoaded", function() {
     const linkTopo = document.getElementById('link-topo');
 
-    // Função para mostrar ou esconder o botão
+    
     const toggleButtonVisibility = () => {
-        if (window.pageYOffset > 10) { // 100px de rolagem para mostrar o botão
-            linkTopo.style.display = 'block'; // Mostra o botão
+        if (window.pageYOffset > 10) { 
+            linkTopo.style.display = 'block'; 
         } else {
-            linkTopo.style.display = 'none'; // Esconde o botão
+            linkTopo.style.display = 'none'; 
         }
     };
 
